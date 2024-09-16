3c15ac636da26c0b5d93616be72f71e3a71004ad
Bilal160 <63717309+Bilal160@users.noreply.github.com>
2024-09-16 17:49:07 +0500

    Update profileController.js

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -9,7 +9,7 @@ const { S3Client } = require("@aws-sdk/client-s3");
 const multerS3 = require("multer-s3");
 const s3 = new S3Client({
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },
   region: process.env.AWS_REGION,
